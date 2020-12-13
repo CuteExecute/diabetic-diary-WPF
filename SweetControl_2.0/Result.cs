900a2bb02e939c3ba91188836046039905349667
Spark <sparkun@mail.ru>
2020-12-13 17:23:39 +0300

    Creating data class - Result

---
+++ SweetControl_2.0/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweetControl_2._0
+{
+    class Result
+    {
+        event Action NewResultNotification;
+
+        string Day { get; set; }
+        string Time { get; set; }
+        int CurrenеDayIndex { get; set; }
+        double Resultation { get; set; }
+
+        public Result(double Resultation, int CurrenеDayIndex)
+        {
+            this.Resultation = Resultation;
+            this.CurrenеDayIndex = CurrenеDayIndex;
+
+            Day = DateTime.Now.Date.ToString("dd.MM.yyyy");
+            Time = DateTime.Now.ToString("HH:mm");
+
+            if (NewResultNotification != null)
+                NewResultNotification.Invoke(); // Notify for creating result 
+        }
+    }
+}
